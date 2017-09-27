41980028a5f3a940a19526ce0e30c9a23691da91
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2017-09-27 09:00:44 +0200

    PUT

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -3,13 +3,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Person;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\HttpFoundation\Response;
+
 
 /**
  * Class ApiController
@@ -21,7 +21,7 @@ class ApiController extends FOSRestController
 {
 
     /**
-     * @Rest\Get(path = "/people", name = "api_people")
+     * @Rest\Get(path = "/people", name = "api_GET_all")
      * @Rest\View()
      */
     public function indexAction()
@@ -32,22 +32,22 @@ class ApiController extends FOSRestController
     /**
      * @Rest\Get(
      *     path="/people/{id}",
-     *     name="api_people_id",
+     *     name="api_GET",
      *     requirements={"id"="\d+"}
      * )
      * @Rest\View
      */
-    public function druidAction(Person $person)
+    public function personAction(Person $person)
     {
         return $person;
     }
 
     /**
-     * @Rest\Post(path = "/people", name = "api_people_add")
+     * @Rest\Post(path = "/people", name = "api_POST")
      * @Rest\View(StatusCode = Response::HTTP_CREATED)
      * @ParamConverter("person", converter="fos_rest.request_body")
      */
-    public function postDruidAction(Person $person, ConstraintViolationList $validation)
+    public function postPersonAction(Person $person, ConstraintViolationList $validation)
     {
         if($validation->count()) {
             return $this->view($validation, Response::HTTP_BAD_REQUEST);
@@ -60,7 +60,7 @@ class ApiController extends FOSRestController
 
         return $this->view($person, Response::HTTP_CREATED,
             [
-                'Location' => $this->generateUrl('api_people_id', ['id' => $person->getId()])
+                'Location' => $this->generateUrl('api_GET', ['id' => $person->getId()])
             ]
         );
     }
@@ -68,17 +68,51 @@ class ApiController extends FOSRestController
     /**
      * @Rest\Delete(
      *     path="/people/{id}",
-     *     name="api_delete_person",
+     *     name="api_DELETE",
      *     requirements={"id"="\d+"}
      * )
      *
      * @Rest\View(StatusCode = Response::HTTP_NO_CONTENT)
      */
-    public function deleteDruidAction(Person $person)
+    public function deletePersonAction(Person $person)
     {
         $em = $this->getDoctrine()->getManager();
 
         $em->remove($person);
         $em->flush();
     }
+
+    /**
+     * @Rest\Put(path = "/people/{id}", name = "api_put")
+     * @ParamConverter("person", class="AppBundle\Entity\Person")
+     * @ParamConverter("personUpdate", converter="fos_rest.request_body")
+     *
+     * @Rest\View(StatusCode = Response::HTTP_NO_CONTENT)
+     */
+    public function putPersonAction(Person $person, Person $personUpdate, ConstraintViolationList $validation)
+    {
+        if($validation->count()) {
+            return $this->view($validation, Response::HTTP_BAD_REQUEST);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->merge($this->updatePerson($person, $personUpdate));
+        $em->flush();
+
+        return $this->view($person, Response::HTTP_NO_CONTENT,
+            [
+                'Location' => $this->generateUrl('api_GET', ['id' => $person->getId()])
+            ]
+        );
+    }
+
+    private function updatePerson(Person $person, Person $personNew)
+    {
+        $person->setName($personNew->getName());
+        $person->setMail($personNew->getMail());
+
+        return $person;
+    }
+
 }
